61d05ec9d05ad83575c60fbb2d9696b0551ff91b
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-06-21 13:22:54 +0300

    Complited task #1.

--- task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
+++ task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
@@ -69,6 +69,46 @@ public class Employee {
     }
 
     public static class EmployeeBuilder {
-        // Write your code here!
+
+        private Employee employee;
+
+        public EmployeeBuilder() {
+            employee = new Employee();
+        }
+
+        public EmployeeBuilder setName(String name) {
+            employee.name = name;
+            return this;
+        }
+
+        public EmployeeBuilder setLastName(String lastName) {
+            employee.lastName = lastName;
+            return this;
+        }
+
+        public EmployeeBuilder setPosition(String position) {
+            employee.position = position;
+            return this;
+        }
+
+        public EmployeeBuilder setPhone (String phone) {
+            employee.phone = phone;
+            return this;
+        }
+
+        public EmployeeBuilder setEmail (String email) {
+            employee.email = email;
+            return this;
+        }
+
+        public EmployeeBuilder setCarnumber (String carNumber) {
+            employee.carNumber = carNumber;
+            return this;
+        }
+
+        public Employee build () {
+            return employee;
+        }
     }
 }
+
