cbf7d3816a254a10c56e3672a4543410820248cc
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-06-21 16:28:14 +0300

    Complited task #3.

--- task3-factory-pattern/src/main/java/com/mjc/stage2/ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/ConnectionFactory.java
@@ -1,7 +1,9 @@
 package com.mjc.stage2;
 
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public interface ConnectionFactory {
-    Connection createConnection();
+    Connection createConnection() throws IOException, SQLException;
 }
--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,24 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+import org.h2.mode.FunctionsPostgreSQL;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory  implements ConnectionFactory {
+    @Override
+    public Connection createConnection() throws IOException, SQLException {
+        Properties properties = new Properties();
+        properties.load(H2ConnectionFactory.class.getClassLoader().getResourceAsStream("app.properties"));
+
+        String url = properties.getProperty("db_url");
+        String user = properties.getProperty("user");
+        String password = properties.getProperty("password");
+        return DriverManager.getConnection(url, user, password);
+    }
 }
 
